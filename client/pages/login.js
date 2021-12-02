29d6dcd72d69ddd656b14a134c1e4d506a2e439a
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-12-02 20:42:25 +0530

    frontend for login page

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,9 +1,96 @@
+import { useState } from 'react';
 import Layout from '../components/Layout';
+import axios from 'axios';
+import { showSuccessMessage, showErrorMessage } from '../helpers/alerts';
+import { API } from '../config';
+import Link from 'next/link';
+import Router from 'next/router';
 
-const Home = () => (
-  <Layout>
-    <div>Hello World!</div>
-  </Layout>
-);
+const Login = () => {
+	const [state, setState] = useState({
+		email: '',
+		password: '',
+		error: '',
+		success: '',
+		buttonText: 'Login',
+	});
 
-export default Home;
+	const { name, email, password, error, success, buttonText } = state;
+
+	const handleChange = (name) => (event) => {
+		setState({
+			...state,
+			[name]: event.target.value,
+			error: '',
+			success: '',
+			buttonText: 'Login',
+		});
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		console.table({ email, password });
+
+		setState({ ...state, buttonText: 'Logging in' });
+
+		try {
+			const response = await axios.post(`${API}/login`, {
+				email,
+				password,
+			});
+		} catch (error) {
+			console.log(error);
+			setState({
+				...state,
+				buttonText: 'Login',
+				error: error.response.data.error,
+			});
+		}
+	};
+
+	const loginForm = () => (
+		<form onSubmit={handleSubmit}>
+			<div className='form-group'>
+				<input
+					value={email}
+					onChange={handleChange('email')}
+					type='email'
+					className='form-control'
+					placeholder='Type your email'
+					required
+				/>
+			</div>
+			<div className='form-group'>
+				<input
+					value={password}
+					onChange={handleChange('password')}
+					type='password'
+					className='form-control'
+					placeholder='Type your password'
+					required
+				/>
+			</div>
+			<div className='form-group'>
+				<button type='submit' className='btn btn-outline-warning'>
+					{buttonText}
+				</button>
+			</div>
+		</form>
+	);
+
+	return (
+		<Layout>
+			{success && showSuccessMessage(success)}
+			{error && showErrorMessage(error)}
+			<div className='col-md-6 offset-md-3'>
+				<h1>Login</h1>
+				<br />
+				{loginForm()}
+				<br />
+				{JSON.stringify(state)}
+			</div>
+		</Layout>
+	);
+};
+
+export default Login;
